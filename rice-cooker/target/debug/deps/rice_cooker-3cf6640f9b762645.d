ad43710829772f82a758d4017b80208dc213b509
Miaritiana <hei.miaritiana@gmail.com>
2023-12-11 11:17:49 +0300

    feat: add d rice-cooker program

---
+++ rice-cooker/target/debug/deps/rice_cooker-3cf6640f9b762645.d
@@ -0,0 +1,5 @@
+/home/soamiandra/cc-d2-my-rice-cooker-Miaritiana/rice-cooker/target/debug/deps/rice_cooker-3cf6640f9b762645: src/main.rs
+
+/home/soamiandra/cc-d2-my-rice-cooker-Miaritiana/rice-cooker/target/debug/deps/rice_cooker-3cf6640f9b762645.d: src/main.rs
+
+src/main.rs:
---
+++ rice-cooker/target/debug/rice-cooker.d
@@ -0,0 +1,1 @@
+/home/soamiandra/cc-d2-my-rice-cooker-Miaritiana/rice-cooker/target/debug/rice-cooker: /home/soamiandra/cc-d2-my-rice-cooker-Miaritiana/rice-cooker/src/main.rs
